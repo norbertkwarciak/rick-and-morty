9cc06984b1bd2d30cdc0f26faad2b9fc487bd578
Norbert Kwarciak <norbert.kwarciak@gmail.com>
2020-01-11 21:39:45 +0100

    Init

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.template.html',
+  filename: './index.html'
+});
+
+const ALIAS_SYMBOL = '@';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              { modules: 'commonjs' }
+            ],
+            '@babel/preset-react'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-transform-runtime'
+          ]
+        }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|mp4|ogg|svg|woff|woff2|ico|eot)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]'
+        }
+      },
+      {
+        test: /^.((?!cssmodule).)*\.(sass|scss)$/,
+        use: [
+          // only use the mode of development
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [autoprefixer, cssnano]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    htmlPlugin,
+    new MiniCssExtractPlugin({
+      filename: 'styles/style.bundle.css'
+    })
+  ],
+  resolve: {
+    alias: {
+      [ALIAS_SYMBOL]: path.resolve(__dirname, './src')
+    }
+  }
+};
